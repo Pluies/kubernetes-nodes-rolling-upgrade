dba5e0e0947eb8b98fc8e79bf8cba2cf7f3ef789
Florent Delannoy <florent.delannoy@gmail.com>
2021-04-08 14:34:13 +0100

    Add script, add README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # kubernetes-nodes-rolling-upgrade
-Script to upgrade worker nodes in a Kubernetes cluster
+
+Script to upgrade worker nodes in a Kubernetes cluster on AWS.
+
+## Assumptions
+
+- You want to upgrade from version _x_ to version _y_
+- Your nodes are in an autoscaling group, and new nodes will be created as version _y_
+
+## Operation
+
+This script will list all nodes running on version _x_ and, one by one:
+- Drain it with `kubectl`
+- Terminate it with `aws ec2 terminate-instance`
+- Wait until the cluster is stable, i.e. no pod in Terminating / Pending / ContainerCreating state
+- Carry on with the next node
+
+**Warning**: this script _does not_ recreate nodes! It only terminates them cleanly and relies on the ASG or the cluster-autoscaler to recreate updated nodes.
+
+## Prerequisites
+
+- [kubectl](https://kubernetes.io/docs/reference/kubectl/kubectl/)
+- [aws-cli](https://aws.amazon.com/cli/)
+
+## Usage
+
+```bash
+VERSION=v1.19.6 ./rolling-upgrade.sh
+```
+
+## Configuration
+
+Configuration is done through env vars.
+
+| Environment variable   | Action                                            | Default value |
+|------------------------|---------------------------------------------------|---------------|
+| `VERSION`              | The version to upgrade to. Must be in the "VERSION" field of `kubectl get node` | _Unset_ |
+| `DRY_RUN`              | Whether to actually delete the nodes, or just print out what the script would do. Any non-empty value forces a dry run. | `""` |
+| `DRAIN_TIMEOUT`        | Maximum time for a node to finish draining, in seconds. | `120` |
---
+++ rolling-upgrade.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Perform a rolling upgrade on a Kubernetes cluster.
+#
+# See README.md for details
+
+VERSION=${VERSION:-noversion}
+DRY_RUN=${1:-}
+DRAIN_TIMEOUT=${DRAIN_TIMEOUT:-120}
+
+if [ "$VERSION" == "noversion" ]
+then
+  echo "Missing env var VERSION"
+  echo "This is the version you want to end up with; e.g. run \`VERSION=1.19 $0\` if you want to upgrade from 1.18 to 1.19."
+  exit 1
+fi
+
+function run() {
+  echo "$@"
+  if [ -z "$DRY_RUN" ]; then
+    "$@"
+  fi
+}
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+while true
+do
+  echo "Looking for upgradeable nodes..."
+  UPGRADEABLE_NODES=$(kubectl get node --no-headers | { grep -v "$VERSION" || true; } | awk '{print $1}')
+  if [ -z "$UPGRADEABLE_NODES" ]
+  then
+    echo "No more upgradeable nodes - rollout finished!"
+    exit 0
+  else
+    echo "Found the following upgradeable nodes:"
+    echo "$UPGRADEABLE_NODES"
+  fi
+
+  echo ""
+  echo "Upgrading all nodes. 🚀"
+
+  for NODE in $UPGRADEABLE_NODES
+  do
+    echo "Upgrading node $NODE"
+
+    echo "${bold}Step 1: drain${normal}"
+    run kubectl drain --timeout="$DRAIN_TIMEOUT"s --ignore-daemonsets --delete-local-data "$NODE" || echo "Drain went over timeout, terminated"
+
+    echo "${bold}Step 2: terminate${normal}"
+    INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=private-dns-name,Values=$NODE" --output text --query 'Reservations[*].Instances[*].InstanceId')
+    if [ -z "$INSTANCE_ID" ]
+    then
+      echo "Instance disappeared, skipping"
+      continue
+    fi
+    run aws ec2 terminate-instances --instance-ids="$INSTANCE_ID"
+
+    echo "${bold}Step 3: wait for pending pods${normal}"
+    PODS=$(kubectl get pods --all-namespaces)
+    while echo "$PODS" | grep -e 'Pending\|ContainerCreating\|Terminating'
+    do
+      echo "^ Found pending / terminating pods, waiting 5 seconds..."
+      sleep 5
+      PODS=$(kubectl get pods --all-namespaces)
+    done
+    echo "No unscheduled pods!"
+
+    echo ""
+  done
+done
